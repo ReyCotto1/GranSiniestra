c9ba415e628211224f5e6650e70e5e2b12c5df95
ReyCotto1 <37230811+ReyCotto1@users.noreply.github.com>
2020-07-16 11:37:58 -0400

    Update GranSiniestraCal.js

--- GranSiniestraCal.js
+++ GranSiniestraCal.js
@@ -20,9 +20,9 @@ function calendar(month) {
             totalFeb = 28;
         }
     }
-    var monthNames = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    var monthNames = ["Niellëx", "Aghas", "Mara", "Kathon", "Nerwe", "Kurda"];
     var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
-    var totalDays = ["31", "" + totalFeb + "", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
+    var totalDays = ["30", "30", "30", "30", "30", "30"];
     var tempDate = new Date(tempMonth + ' 1 ,' + year);
     var tempweekday = tempDate.getDay();
     var tempweekday2 = tempweekday;
